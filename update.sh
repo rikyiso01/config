86dff56dbb2d81014e5369508381186498466a0a
rikyiso01 <riky.isola@gmail.com>
2022-07-18 17:35:48 +0200

    fix update

--- update.sh
+++ update.sh
@@ -8,12 +8,12 @@ SYSTEM='/etc/nixos'
 LOCAL="$HOME/.config/nixpkgs"
 MONITORED_LOCALS="$LOCAL/home.nix"
 MONITORED_SYSTEMS="$SYSTEM/configuration.nix"
-OLD_LOCAL="$(cat $MONITORED_LOCALS)"
-OLD_SYSTEM="$(cat $MONITORED_SYSTEMS)"
+OLD_LOCALS="$(cat $MONITORED_LOCALS)"
+OLD_SYSTEMS="$(cat $MONITORED_SYSTEMS)"
 
-git stash
+git -C "$LOCAL" stash
 git -C "$LOCAL" pull
-git stash pop || true
+git -C "$LOCAL" stash pop || true
 if [[ "$(cat $MONITORED_SYSTEMS)" != "$OLD_SYSTEMS" ]]
 then
     sudo git -C "$SYSTEM" pull
