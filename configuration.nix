fca627b2c7530e26bfe684434c0dd7ea5e8b5dd2
rikyiso01 <riky.isola@gmail.com>
2022-07-19 20:39:49 +0200

    add jupyter

--- configuration.nix
+++ configuration.nix
@@ -175,4 +175,33 @@
   };
 
   programs.gnupg.agent.enable=true;
+
+  services.jupyter= {
+    enable=true;
+    kernels={
+      python3 = let
+        env = (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [
+                ipykernel
+                pandas
+                scipy
+                numpy
+                plotly
+                matplotlib
+              ]));
+      in {
+        displayName = "Python 3";
+        argv = [
+          "${env.interpreter}"
+          "-m"
+          "ipykernel_launcher"
+          "-f"
+          "{connection_file}"
+        ];
+        language = "python";
+        logo32 = "${env.sitePackages}/ipykernel/resources/logo-32x32.png";
+        logo64 = "${env.sitePackages}/ipykernel/resources/logo-64x64.png";
+      };
+    };
+  };
+
 }
