836bdce81420993a091ca75eff296f5c43d4f121
rikyiso01 <riky.isola@gmail.com>
2022-07-21 11:10:59 +0200

    revert to ungoogled chromium

--- home.nix
+++ home.nix
@@ -36,7 +36,7 @@
     materia-theme
     docker-compose
     nodejs
-    chromium
+    ungoogled-chromium
     (let 
       my-python-packages = python-packages: with python-packages; [
         poetry
