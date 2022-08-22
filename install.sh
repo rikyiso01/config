4d9e5cca58fb35f89c098daff04c14987b10dee1
rikyiso01 <riky.isola@gmail.com>
2022-08-22 09:58:17 +0200

    switch to unstable channel

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo nix-channel --update
 
 sudo cp -rf * .git '/etc/nixos'
 
-nix-channel --add 'https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz' home-manager
+nix-channel --add 'https://github.com/nix-community/home-manager/archive/master.tar.gz' home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 nix-shell '<home-manager>' -A install
