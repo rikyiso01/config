531a0b7a040cd003e81e4bf3e076bd335321a4e9
rikyiso01 <riky.isola@gmail.com>
2022-08-23 21:26:15 +0200

    restore theme

--- home.nix
+++ home.nix
@@ -103,14 +103,13 @@ rec {
       enable = true;
       plugins = [
         { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-        # { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
       ];
     };
 
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" "sudo"];
-      theme="sorin";
     };
   };
 
