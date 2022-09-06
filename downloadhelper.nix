77079bcd8a1f0fe68da055bd92c614a6c0cd6e17
rikyiso01 <riky.isola@gmail.com>
2022-09-06 09:17:59 +0200

    trying to add derivations

--- downloadhelper.nix
+++ downloadhelper.nix
@@ -1,15 +1,20 @@
-{ stdenv, fetchzip }:
+{ stdenv, fetchzip, autoPatchelfHook }:
 stdenv.mkDerivation rec {
   name    = "downloadhelper-${version}";
-  version = "1.6.2";
+  version = "1.6.3";
 
   src = fetchzip {
     url = "https://github.com/mi-g/vdhcoapp/releases/download/v${version}/net.downloadhelper.coapp-${version}-1_amd64.tar.gz";
     sha256 = "sha256:0kjwagwql3b6im4hmr83jjyx8ngwg0rc49xx47vkvislbfybby18";
   };
 
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+
+  sourceRoot = ".";
+
   installPhase = ''
-    mkdir -p $out/bin
     install -Dm555 -t $out/bin bin/net.downloadhelper.coapp-linux-64
   '';
 }
