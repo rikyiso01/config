b384bbff9e11379ab507a305b7b208a2b8da6879
rikyiso01 <riky.isola@gmail.com>
2022-09-14 18:04:51 +0200

    removes wireguard

--- home.nix
+++ home.nix
@@ -28,7 +28,6 @@ rec {
     gnomeExtensions.gsconnect
     gnomeExtensions.disconnect-wifi
     gnomeExtensions.refresh-wifi-connections
-    gnomeExtensions.wireguard-indicator
     file
     roboto
     fira-code
