34f7e69206a773722f4963f6caa101d8f57bd7af
rikyiso01 <riky.isola@gmail.com>
2022-10-22 09:43:00 +0200

    trying to increase battery life with tlp

--- configuration.nix
+++ configuration.nix
@@ -185,7 +185,30 @@
   services.power-profiles-daemon.enable = false;
   services.gnome.tracker-miners.enable = false;
   services.thermald.enable = true;
-  services.tlp.settings = { };
+  services.tlp.settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    CPU_MAX_PERF_ON_BAT = 30;
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    CPU_HWP_ON_BAT = "power";
+    CPU_BOOST_ON_BAT = 0;
+    SCHED_POWERSAVE_ON_BAT = 1;
+    CPU_HWP_DYN_BOOST_ON_BAT = 0;
+    DISK_IOSCHED = "noop noop";
+    SATA_LINKPWR_ON_BAT = "min_power min_power";
+    PCIE_ASPM_ON_BAT = "powersupersave";
+    AHCI_RUNTIME_PM_TIMEOUT = 15;
+    WIFI_PWR_ON_BAT = "on";
+    WOL_DISABLE = "Y";
+    SOUND_POWER_SAVE_ON_BAT = "1";
+    SOUND_POWER_SAVE_CONTROLLER = "Y";
+    RUNTIME_PM_ON_BAT = "auto";
+    USB_AUTOSUSPEND = 1;
+    DEVICES_TO_DISABLE_ON_BAT = "bluetooth";
+    INTEL_GPU_MAX_FREQ_ON_BAT = 800;
+    INTEL_GPU_BOOST_FREQ_ON_BAT = 800;
+    MAX_LOST_WORK_SECS_ON_BAT = 180;
+  };
 
   networking.hosts = {
     "0.0.0.0" = [
