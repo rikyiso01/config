294fc5941a43063bbd7dd3a9b8f8f408c19a8175
rikyiso01 <riky.isola@gmail.com>
2022-10-22 09:04:00 +0200

    fix battery usage

--- configuration.nix
+++ configuration.nix
@@ -185,11 +185,7 @@
   services.power-profiles-daemon.enable = false;
   services.gnome.tracker-miners.enable = false;
   services.thermald.enable = true;
-  services.tlp.settings = {
-    CPU_SCALING_GOVERNOR_ON_AC = false;
-    CPU_SCALING_GOVERNOR_ON_BAT = false;
-  };
-  services.auto-cpufreq.enable = true;
+  services.tlp.settings = { };
 
   networking.hosts = {
     "0.0.0.0" = [
