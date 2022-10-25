3a741d366fbd52f43b915b8cab4621626ac8edfe
rikyiso01 <riky.isola@gmail.com>
2022-10-25 09:33:05 +0200

    fix game backup script

--- game-backup.sh
+++ game-backup.sh
@@ -2,18 +2,4 @@
 
 set -euo pipefail
 
-rm -rf '/tmp/backup'
-mkdir -p '/tmp/backup'
-
-mkdir '/tmp/backup/net.lutris.Lutris'
-cp -r "$HOME/.var/app/net.lutris.Lutris/data" '/tmp/backup/net.lutris.Lutris'
-cp -r "$HOME/.var/app/net.lutris.Lutris/config" '/tmp/backup/net.lutris.Lutris'
-
-mkdir '/tmp/backup/org.libretro.RetroArch'
-cp -r "$HOME/.var/app/org.libretro.RetroArch/config" '/tmp/backup/org.libretro.RetroArch'
-
-mkdir '/tmp/backup/Games'
-cp -r "$HOME/Games" '/tmp/backup/Games'
-
-zip -ry './backup.zip' '/tmp/backup'
-rm -rf '/tmp/backup'
+zip -vry './backup.zip' "$HOME/.var/app/net.lutris.Lutris/data" "$HOME/.var/app/net.lutris.Lutris/config" "$HOME/.var/app/org.libretro.RetroArch/config" "$HOME/Games"
