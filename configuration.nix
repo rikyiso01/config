e99db9098006f9390abd596d890b973eeec92846
rikyiso01 <riky.isola@gmail.com>
2022-10-28 21:24:24 +0200

    add kde for testing

--- configuration.nix
+++ configuration.nix
@@ -58,6 +58,8 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  services.xserver.desktopManager.plasma5.enable = true;
+
   # Configure keymap in X11
   services.xserver = {
     layout = "it";
