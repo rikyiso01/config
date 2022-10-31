fa31f50d0e2ceb68187e82c2801da7b9679b8585
rikyiso01 <riky.isola@gmail.com>
2022-10-31 12:01:54 +0100

    moving to manjaro

--- flatpak.sh
+++ flatpak.sh
@@ -2,18 +2,6 @@
 
 set -euo pipefail
 
-fix()
-{
-    grep -v 'DBusActivatable=true' < "$HOME/.local/share/flatpak/exports/share/applications/$1.desktop" > "$HOME/.local/share/applications/$1.desktop"
-    chmod +x "$HOME/.local/share/applications/$1.desktop"
-}
-
-add-wayland()
-{
-    sed 's/Exec=.*/& --enable-features=UseOzonePlatform --ozone-platform=wayland/g' < "$HOME/.local/share/flatpak/exports/share/applications/$1.desktop" > "$HOME/.local/share/applications/$1.desktop"
-    chmod +x "$HOME/.local/share/applications/$1.desktop"
-}
-
 flatpak remote-add --user --if-not-exists flathub 'https://flathub.org/repo/flathub.flatpakrepo'
 
 flathub='
@@ -21,16 +9,6 @@ com.github.marktext.marktext
 com.brave.Browser
 com.google.AndroidStudio
 com.github.tchx84.Flatseal
-org.gnome.Evince
-org.gnome.FileRoller
-org.gnome.Shotwell
-org.gnome.baobab
-org.gnome.TextEditor
-org.gnome.seahorse.Application
-org.gnome.Characters
-org.gnome.Logs
-com.github.tchx84.Flatseal
-org.gnome.dfeet
 org.videolan.VLC
 org.gimp.GIMP
 com.visualstudio.code
@@ -53,21 +31,7 @@ then
     ln -s "$HOME/.nix-profile/share/fonts" "$HOME/.local/share/fonts"
 fi
 
-#if [ ! -d "$HOME/.themes" ]
-#then
-#    ln -s "$HOME/.nix-profile/share/themes" "$HOME/.themes"
-#fi
-
 flatpak install --user --or-update -y flathub $flathub
-
-fix 'org.gnome.baobab'
-fix 'org.gnome.FileRoller'
-fix 'org.gnome.TextEditor'
-fix 'org.gnome.seahorse.Application'
-fix 'org.gnome.Characters'
-fix 'org.gnome.Logs'
-
-#flatpak override --user --filesystem='~/.themes:ro'
 
 flatpak override 'com.visualstudio.code' --user --env="PATH=$HOME/.local/bin:$HOME/.local/flatpak/:$HOME/.nix-profile/bin:/app/bin:/usr/bin:$HOME/.var/app/com.visualstudio.code/data/node_modules/bin"
 
