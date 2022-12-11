18648ca07473adf0ef0aed71d4c0bf45def69b68
rikyiso01 <riky.isola@gmail.com>
2022-12-11 10:53:05 +0100

    add julia and poetry

--- home.nix
+++ home.nix
@@ -80,13 +80,13 @@ rec {
     nano
     vim
     tlp
-    #julia
     traceroute
+    poetry
+    julia-bin
     (pkgs.callPackage ./downloadhelper.nix { })
     (
       let
         my-python-packages = python-packages: with python-packages; [
-          #poetry
           pudb
           ipython
           ipykernel
