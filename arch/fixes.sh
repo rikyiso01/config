a1d86561b416752491987270c60d3c3dd6fe5ac0
Riccardo Isola <riky.isola@gmail.com>
2026-01-01 18:26:27 +0100

    remove script for fixed system bug

--- arch/fixes.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-
-sudo pacman -U https://archive.archlinux.org/packages/g/systemd/systemd-258-2-x86_64.pkg.tar.zst https://archive.archlinux.org/packages/g/systemd/systemd-libs-258-2-x86_64.pkg.tar.zst https://archive.archlinux.org/packages/g/systemd/systemd-sysvcompat-258-2-x86_64.pkg.tar.zst
