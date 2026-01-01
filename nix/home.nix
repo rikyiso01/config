39a72a6fc03ebfa6c289233b9458ea5d16b23345
Riccardo Isola <riky.isola@gmail.com>
2026-01-01 18:32:40 +0100

    temporary fix neovim treesitter update bug

--- nix/home.nix
+++ nix/home.nix
@@ -447,17 +447,17 @@ let
                 ['<esc>'] = cmp.mapping.abort(),
             },
         }
-        require'nvim-treesitter.configs'.setup {
-          highlight = {
-            enable = true,
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-          },
-          matchup={enable=true},
-        }
+        -- require'nvim-treesitter.configs'.setup {
+        --   highlight = {
+        --     enable = true,
+        --     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        --     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        --     -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        --     -- Instead of true it can also be a list of languages
+        --     additional_vim_regex_highlighting = false,
+        --   },
+        --   matchup={enable=true},
+        -- }
         require('gitblame').setup {
             enabled = true,
             message_when_not_committed = ""
