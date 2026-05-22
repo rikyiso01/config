1ce8d276c306d11d1e2c9c3b09ea0acb5871d85b
rikyiso01 <rikyiso01@noreply.codeberg.org>
2026-05-22 20:43:29 +0200

    update cleanup script

--- nix/cleanup.sh
+++ nix/cleanup.sh
@@ -4,5 +4,9 @@ set -euo pipefail
 
 podman system prune --all --volumes --force
 podman rmi --all --force
+/bin/find ~/backup -type d -name ".direnv" -exec rm -r {} ';' || true
+/bin/find ~/Work -type d -name ".direnv" -exec rm -r {} ';' || true
 nix-collect-garbage --delete-old
 nix-store --optimise
+rm -r ~/Downloads/* || true
+trash-empty -f
