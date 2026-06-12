6c40bb77dc329a56f05d89611faaae534128d2d7
rikyiso01 <rikyiso01@noreply.codeberg.org>
2026-06-12 17:22:02 +0200

    fix work script

--- nix/work.fish
+++ nix/work.fish
@@ -6,11 +6,11 @@ set migrations ~/Work/FERRARI/migrations/ski
 ssh homeassistant.riccardoisola.dev docker start teams
 
 hyprctl dispatch exec flatpak run io.github.ungoogled_software.ungoogled_chromium
-sleep 1
+sleep 2
 hyprctl dispatch movetoworkspace 2
 
 hyprctl dispatch exec flatpak run org.remmina.Remmina ~/.var/app/org.remmina.Remmina/data/remmina/group_rdp_work-laptop-remote_homeassistant-riccardoisola-dev.remmina
-sleep 1
+sleep 2
 hyprctl dispatch movetoworkspace 3
 hyprctl dispatch exec flatpak run io.dbeaver.DBeaverCommunity
 
@@ -19,7 +19,7 @@ tmux new-window -c $workspace \; send-keys 'nvim Controllers/HomeController.cs' 
 tmux new-window -c $workspace \; send-keys 'dotnet watch run' 'Enter' \; split-window -h -c $workspace \; send-keys 'dotnet run -p ../../Reply.Ferrari.MQ.AppLandingAuth' 'Enter' \; select-pane -t 0 \; resize-pane -Z
 tmux new-window -c $migrations \; send-keys 'nvim' 'Enter' \; split-window -h -c $migrations \; select-pane -t 0 \; resize-pane -Z
 tmux new-window -t 5 -c ~ \; send-keys 'ncmpcpp' 'Enter' \; split-window -h -c ~ \; send-keys 'ssh -N homeassistant.riccardoisola.dev -L1144:localhost:1144' 'Enter' \; select-pane -t 0 \; resize-pane -Z
-sleep 1
+sleep 2
 
 flatpak run io.gitlab.librewolf-community --new-tab http://127.0.0.1:1144/vnc.html
 flatpak run io.gitlab.librewolf-community --new-tab https://localhost:7202
