de7849ef676d02657bd0f0880bc042eb2e3f99e4
rikyiso01 <rikyiso01@noreply.codeberg.org>
2026-07-17 17:01:46 +0200

    update quit work

--- nix/quit-work.fish
+++ nix/quit-work.fish
@@ -3,7 +3,14 @@
 hyprctl dispatch workspace 2
 sleep 1
 hyprctl dispatch killactive
-sleep 3
+hyprctl dispatch workspace 3
+sleep 1
+hyprctl dispatch killactive
+hyprctl dispatch workspace 4
+sleep 1
+hyprctl dispatch killactive
+hyprctl dispatch workspace 5
+sleep 1
 hyprctl dispatch killactive
 
 tmux select-window -t 1 \; select-pane -t 0 \; send-keys ':q' 'Enter'
